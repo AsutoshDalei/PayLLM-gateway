9da480d8039827d2f0bd8cfa0652be086ef79d55
Asutosh Dalei <asutoshdalei@gmail.com>
2025-02-10 20:24:28 -0500

    working on service class

--- functionCallingSystem.py
+++ functionCallingSystem.py
@@ -8,6 +8,40 @@ from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 # Importing model
 llm = ChatOllama(model="llama3.2", temperature=0)
 
+mockElectricityBills = {
+    "9182":{'Customer Name':'Asutosh Dalei','service provider':'TS Elec Board','unit':32,'Amount':341, "Due Date":'10/01/2025', 'status':'Paid'},
+    "1928":{'Customer Name':'Asutosh Dalei','service provider':'TS Elec Board','unit':37,'Amount':547, "Due Date":'11/02/2025','status':'Unpaid'},
+    "1038":{'Customer Name':'Asutosh Dalei','service provider':'TS Elec Board','unit':23,'Amount':298, "Due Date":'09/10/2025','status':'Paid'}
+}
+
+mockWifiBills = {
+    "8321":{'Customer Name':'Asutosh Dalei','service provider':'Airtel','Amount':1008, "Due Date":'04/03/2025', 'status':'Paid'},
+    "1928":{'Customer Name':'Asutosh Dalei','service provider':'Airtel','Amount':1276, "Due Date":'04/02/2025','status':'Paid'},
+    "1038":{'Customer Name':'Asutosh Dalei','service provider':'Airtel','Amount':1932, "Due Date":'08/04/2025','status':'Unpaid'}
+}
+
+class BillPay():
+    def __init__(self, service, llm, billDB):
+        serviceTools = []
+        self.service = service
+        self.llmService = llm.bind_tools(serviceTools)
+
+        self.userDetails = fetchUserDetails()
+        self.billDB = billDB
+
+    @tool
+    def fetchBill(self, billNumber):
+        """Function to fetch bill details using bill number. Input: Bill Number. Output: Bill Amount, Bill Details"""
+        # This is a mock setup, for the sole purpose of testing the system.
+        return json.dumps(self.billDB[billNumber])
+    
+    def payBill(self, billNumber):
+        """Function to pay bill using bill number. Input: Bill Number. Output: Bill paid status"""
+         # This is a mock setup, for the sole purpose of testing the system.
+        if self.billDB[billNumber]
+        
+
+
 # Creating Tools
 @tool
 def fetchUserDetails() -> str:
@@ -41,6 +75,7 @@ llm = llm.bind_tools(tools)
 
 # Central session messages
 messages = [SystemMessage(content = "You are a helpful bill payment assistant for a user. Start by greeting the user.")]
+global messages
 aiMsg = llm.invoke(messages)
 messages.append(aiMsg)
 
